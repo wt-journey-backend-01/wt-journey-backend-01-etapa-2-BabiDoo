cd9020ffb953b89093bc03ade2139dd2c35a5a7b
Barbara <barbara.laisdorneles@gmail.com>
2025-07-30 09:47:54 -0300

    routers: add agent endpoints

---
+++ routes/agentRoutes.js
@@ -0,0 +1,13 @@
+import { express } from 'express';
+import * as controller from '../controllers/agentController.js';
+
+const router = express.Router();
+
+router.get('/', controller.getAllAgents);
+router.get('/:id', controller.getAgentById);
+router.post('/', controller.createAgent);
+router.put('/:id', controller.updateAgent);
+router.patch('/:id', controller.patchAgent);
+router.delete('/:id', controller.deleteAgent);
+
+export { router as agentRouter };

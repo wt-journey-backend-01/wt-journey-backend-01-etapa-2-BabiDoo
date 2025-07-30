8f57cd1041711482b56ddca45dd0dee40c60f9e4
Barbara <barbara.laisdorneles@gmail.com>
2025-07-30 12:08:07 -0300

    add errorHandler middleware

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import 'dotenv/config'; //middleware de config da biblioteca dotenv
 import express from 'express';
 import agentesRoutes from './routes/agentesRoutes.js';
 import casosRoutes from './routes/casosRoutes.js';
+import { errorHandler } from './utils/errorHandler.js'
 
 const app = express();
 const PORT = process.env.PORT || 3000; //usa a variavel PORT que esta definida no arquivo .env
 app.use(express.json()); //middleware do express para lidar com dados do tipo json
 app.use('/agentes', agentesRoutes);
 app.use('/casos', casosRoutes);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));

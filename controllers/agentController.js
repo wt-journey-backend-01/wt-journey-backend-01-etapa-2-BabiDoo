c41e3381caf06f043dcb485d3897e37f526f3f8c
Barbara <barbara.laisdorneles@gmail.com>
2025-07-30 09:57:11 -0300

    controllers: agent and case

---
+++ controllers/agentController.js
@@ -0,0 +1,79 @@
+import * as repository from '../repositories/agentRepository';
+import { agentSchema } from '../utils/agentValidation';
+import { patchSchema } from '../utils/partialDataValidation.js';
+
+class ApiError extends Error {
+    constructor(message, statusCode = 500) {
+        super(message);
+        this.name = 'ApiError';
+        this.statusCode = statusCode;
+    }
+}
+
+const getAllAgents = (req, res, next) => {
+    try {
+        const agents  = repository.findAll();
+        res.status(200).json(agents)
+    } catch (error) {
+        next( new ApiError ('Não foi possível listar os agentes'));
+    }
+};
+
+const getAgentById = (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const agent = repository.findById(id);
+    if (!agent) return next(new ApiError('Agente não encontrado.', 404));
+    res.status(200).json(agent);
+  } catch (error) {
+    next(new ApiError(error.message, 500));
+  }
+};
+
+const createAgent = (req, res, next) => {
+    try {
+        const data = agentSchema.parse(req.body);
+        const agent = repository.create(data);
+        res.status(201).json(agent);
+    } catch (error) {
+        next(new ApiError('Não foi possível criar um agente', 400));
+    }
+};
+
+const updateAgent = (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const data = agentSchema.parse(req.body);
+        const updated = repository.update(id, data);
+        if (!updated) return next(new ApiError('Agente não encontrado.', 404));
+        res.status(200).json(updated);
+    } catch (error) {
+        next(new ApiError('Erro ao atualizar o agente.', 400));
+    }
+};
+
+const patchAgent = (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const partialData = patchSchema.parse(req.body);
+    const updated = repository.patch(id, partialData);
+    if (!updated) return next(new ApiError('Agente não encontrado.', 404));
+    res.status(200).json(updated);
+  } catch (error) {
+    next(new ApiError('Erro ao atualizar o agente', 400));
+  }
+};
+
+
+const deleteAgent = (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const deleted = repository.remove(id);
+        if (!deleted) return next(new ApiError('Agente não encontrado.', 404));
+        res.sendStatus(204);
+    } catch (error) {
+        next(new ApiError('Erro ao deletar agente.'));
+    }
+};
+
+export { getAllAgents, getAgentById, createAgent, updateAgent, patchAgent, deleteAgent };
---
+++ controllers/caseController.js
@@ -0,0 +1,79 @@
+import * as repository from '../repositories/caseRepository';
+import { caseSchema } from '../utils/caseValidation';
+import { patchSchema } from '../utils/partialDataValidation.js';
+
+class ApiError extends Error {
+    constructor(message, statusCode = 500) {
+        super(message);
+        this.name = 'ApiError';
+        this.statusCode = statusCode;
+    }
+}
+
+const getAllCases = (req, res, next) => {
+    try {
+        const cases  = repository.findAll();
+        res.status(200).json(cases)
+    } catch (error) {
+        next( new ApiError ('Não foi possível listar os casos'));
+    }
+};
+
+const getCaseById = (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const caseItem = repository.findById(id);
+    if (!caseItem) return next(new ApiError('Caso não encontrado.', 404));
+    res.status(200).json(caseItem);
+  } catch (error) {
+    next(new ApiError(error.message, 500));
+  }
+};
+
+const createCase = (req, res, next) => {
+    try {
+        const data = caseSchema.parse(req.body);
+        const createdCase = repository.create(data);
+        res.status(201).json(createdCase);
+    } catch (error) {
+        next(new ApiError('Não foi possível criar este caso', 400));
+    }
+};
+
+const updateCase = (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const data = caseSchema.parse(req.body);
+        const updated = repository.update(id, data);
+        if (!updated) return next(new ApiError('Caso não encontrado.', 404));
+        res.status(200).json(updated);
+    } catch (error) {
+        next(new ApiError('Erro ao atualizar o caso.', 400));
+    }
+};
+
+const patchCase = (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const partialData = patchSchema.parse(req.body);
+    const updated = repository.patch(id, partialData);
+    if (!updated) return next(new ApiError('Caso não encontrado.', 404));
+    res.status(200).json(updated);
+  } catch (error) {
+    next(new ApiError('Erro ao atualizar o caso.', 400));
+  }
+};
+
+
+const deleteCase = (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const deleted = repository.remove(id);
+        if (!deleted) return next(new ApiError('Caso não encontrado.', 404));
+        res.sendStatus(204);
+    } catch (error) {
+        next(new ApiError('Erro ao deletar o caso.'));
+    }
+};
+
+export { getAllCases, getCaseById, createCase, updateCase, patchCase, deleteCase };

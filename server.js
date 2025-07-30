0c145ad38097071ff2e4757d0e898a0519a0046c
Barbara <barbara.laisdorneles@gmail.com>
2025-07-30 11:41:53 -0300

    fix: change file names and respective exports to portuguese

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import 'dotenv/config'; //middleware de config da biblioteca dotenv
 import express from 'express';
-import agentRoutes from './routes/agentRoutes.js';
-import caseRoutes from './routes/caseRoutes.js';
+import agentesRoutes from './routes/agentesRoutes.js';
+import casosRoutes from './routes/casosRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000; //usa a variavel PORT que esta definida no arquivo .env
 app.use(express.json()); //middleware do express para lidar com dados do tipo json
-app.use('/agents', agentRoutes);
-app.use('/cases', caseRoutes);
+app.use('/agentes', agentesRoutes);
+app.use('/casos', casosRoutes);
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
--- utils/partialDataValidation.js
+++
@@ -1,8 +0,0 @@
-import { agentSchema } from '../utils/agentValidation.js';
-import { caseSchema } from '../utils/caseValidation.js';
-
-const agentPatchSchema = agentSchema.partial();
-const casePatchSchema = caseSchema.partial();
-
-export { agentPatchSchema, casePatchSchema };
-

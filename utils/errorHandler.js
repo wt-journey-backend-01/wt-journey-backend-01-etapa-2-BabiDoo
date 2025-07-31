655d460c638ec4ac8fefec98a702c7269c9838e0
Barbara <barbara.laisdorneles@gmail.com>
2025-07-31 13:48:26 -0300

    adjust erroHandler

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,6 +1,11 @@
 export function errorHandler(err, req, res, next) {
   if (err.name === 'ApiError') {
-    return res.status(err.statusCode).json({ message: err.message });
+    const body = {
+      status: err.statusCode,
+      message: err.message,
+      ...(err.errors && { errors: err.errors }),
+    };
+    return res.status(err.statusCode).json(body);
   }
   console.error(err);
   return res.status(500).json({ message: 'Internal Server Error' });

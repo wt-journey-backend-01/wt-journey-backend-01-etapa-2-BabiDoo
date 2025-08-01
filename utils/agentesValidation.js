efe2693b197096810f38b2c0f11cd490082c4dfd
Barbara <barbara.laisdorneles@gmail.com>
2025-08-01 15:07:07 -0300

    fix validation

--- utils/agentesValidation.js
+++ utils/agentesValidation.js
@@ -4,8 +4,6 @@ const agentesSchema = z.object({
     name: z.string({ required_error: 'Nome é obrigatório.', invalid_type_error: 'Use apenas letras.'}).min(1, 'Nome não pode ser vazio.'),
     incorporationDate: z.string({ required_error: 'A data de incorporação é obrigatória.' }).refine(val => /^\d{4}-\d{2}-\d{2}$/.test(val), {
       message: 'A data deve estar no formato YYYY-MM-DD.',
-    }).refine(val => !isNaN(Date.parse(val)), {
-      message: 'A data deve estar no formato YYYY-MM-DD.',
     }).transform(val => new Date(val)),
     position: z.string({ required_error: 'O cargo é obrigatório.', invalid_type_error: 'Use apenas letras.'}).min(1, 'O cargo não pode ser vazio'),
     agentId: z.uuid({ required_error: 'Uma autoridade responsável é obrigatório.'})

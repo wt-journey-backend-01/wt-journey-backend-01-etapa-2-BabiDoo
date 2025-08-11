a5f264ce577d3fb253343aef9358cf2e79048881
Barbara <barbara.laisdorneles@gmail.com>
2025-08-11 02:34:02 -0300

    Update caseValidation.js

--- utils/caseValidation.js
+++ utils/caseValidation.js
@@ -1,13 +1,15 @@
 import z from 'zod';
 
 const caseSchema = z.object ({
-    title: z.string({ require_error: 'O título do caso é obrigatório.'}).min(10, 'O título do caso não pode ser vazio.'),
-    description: z.string({ require_error: 'A descrição do caso é obrigatória.'}).min(10, 'O título do caso não pode ser vazio.'),
-    status: z.enum(['Aberto', 'Solucionado'], {
+    titulo: z.string({ require_error: 'O título do caso é obrigatório.'}).min(10, 'O título do caso não pode ser vazio.'),
+    descricao: z.string({ require_error: 'A descrição do caso é obrigatória.'}).min(10, 'O título do caso não pode ser vazio.'),
+    status: z.enum(['aberto', 'solucionado'], {
         required_error: 'O status do caso é obrigatório.',
         invalid_type_error: 'Status deve ser "aberto" ou "solucionado".',
 
-})
+    }),
+    agenteId: z.uuid({ require_error: 'Uma autoridade responsável é obrigatório.'})
+
 })
 
 export { caseSchema };

acf3f0941b44f7c04b2f2153790a313141c6db08
Barbara <barbara.laisdorneles@gmail.com>
2025-08-11 02:34:53 -0300

    Update agentValidation.js

--- utils/agentValidation.js
+++ utils/agentValidation.js
@@ -1,15 +1,13 @@
 import z from 'zod';
 
-const caseSchema = z.object ({
-    titulo: z.string({ require_error: 'O título do caso é obrigatório.'}).min(10, 'O título do caso não pode ser vazio.'),
-    descricao: z.string({ require_error: 'A descrição do caso é obrigatória.'}).min(10, 'O título do caso não pode ser vazio.'),
-    status: z.enum(['aberto', 'solucionado'], {
-        required_error: 'O status do caso é obrigatório.',
-        invalid_type_error: 'Status deve ser "aberto" ou "solucionado".',
-
-    }),
-    agenteId: z.uuid({ require_error: 'Uma autoridade responsável é obrigatório.'})
-
+const agentSchema = z.object({
+    nome: z.string({ require_error: 'Nome é obrigatório.', invalid_type_error: 'Use apenas letras.'}).min(1, 'Nome não pode ser vazio.'),
+    dataDeIncorporacao: z.string({ required_error: 'A data de incorporação é obrigatória.' }).refine(val => /^\d{4}-\d{2}-\d{2}$/.test(val), {
+      message: 'A data deve estar no formato YYYY-MM-DD.',
+    }).refine(val => !isNaN(Date.parse(val)), {
+      message: 'A data deve estar no formato YYYY-MM-DD.',
+    }).transform(val => new Date(val)),
+    cargo: z.string({ require_error: 'O cargo é obrigatório.', invalid_type_error: 'Use apenas letras.'}).min(1, 'O cargo não pode ser vazio'),
 })
 
-export { caseSchema };
+export { agentSchema };
